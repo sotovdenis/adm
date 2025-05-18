5edc84d07e45182fe5c1f702c0db88a93d3aa1e8
denissotov <denissotovggg223@gmail.com>
2025-05-18 15:16:02 +0300

    init

---
+++ src/main/java/gpb/adminka/AdminkaApplication.java
@@ -0,0 +1,13 @@
+package gpb.adminka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gpb/adminka/AdminkaApplicationTests.java
@@ -0,0 +1,13 @@
+package gpb.adminka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
